ae9e43ba807c4c830dac579d0f881a70c8a7cff0
Misha <abachyanm@gmail.com>
2021-02-18 04:31:27 +0400

    Like function made

--- src/components/LIkeButton.jsx
+++ src/components/LIkeButton.jsx
@@ -1,22 +1,35 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useContext, useEffect, useMemo, useState} from 'react'
 import {Context} from "./context";
 import firebase from "firebase";
 
 
 const LikeButton = ({id})=>{
-
     const{videos, currentUser} = useContext(Context)
+
+    const[videoId, setVideoId] = useState([])
+    useMemo(getLikedVideos, [currentUser])
+
+
+    function getLikedVideos(){
+        let likedVideosArray =[]
+        if (currentUser){
+            firebase.database().ref(`videos/${currentUser.uid}`).on('value', (snapshot)=>{
+                snapshot.forEach((data)=>{
+                    likedVideosArray = [data.val().video, ...likedVideosArray]
+                    setVideoId(likedVideosArray)
+                })
+            })
+        }
+    }
 
 
     function handleLike(event){
         const likeBtnId = event.target.getAttribute('id')
-
         videos.map((video)=>{
             if (likeBtnId === video.id){
                 const userVideo = firebase.database().ref(`videos/${currentUser.uid}`)
                 const userVideoData = {
                     video: video,
-                    identNumber: currentUser.uid,
                     liked: true
                 }
                 userVideo.push(userVideoData)
@@ -24,10 +37,23 @@ const LikeButton = ({id})=>{
         })
     }
 
-    return (
-        <button className="like" onClick={handleLike} id={id}>
-            Like
-        </button>
+    return(
+        <>
+            <button className="like" onClick={handleLike} id={id}>
+                Like
+            </button>
+            {
+                videoId.map((vid)=>{
+                    if(vid.id === id && currentUser){
+                        return (
+                            <button className="liked" onClick={handleLike} id={id} disabled>
+                                Like
+                            </button>
+                        )
+                    }
+                })
+            }
+        </>
     )
 
 }
